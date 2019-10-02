4ae83c43795b804e38f011f57f2cc9604103681f
Willy Du <willy.du@ulictek.com>
2019-10-02 15:41:00 +0800

    Update for scaling image.

--- process_image.py
+++ process_image.py
@@ -1,20 +1,20 @@
 import cv2
 import os
-import re
+
 path ="..\\waifu2x\\images" #Target of image file
 files = os.listdir(path) #Find all files in this file
 s=[]
+
 for file in files:
     file_path = path+'\\'+file
     if os.path.exists(file_path):#Check wether this file is exist or not
         img_extension = os.path.splitext(file_path)[-1] #Get filename extension
         if img_extension == '.png':
-            print(img_extension)
+            s.append(file_path) 
     else:
         print("Not exist")
-#image=cv2.imread('test.jpg')
-#res=cv2.resize(image,(32,32),interpolation=cv2.INTER_CUBIC)
-#cv2.imshow('iker',res)
-#cv2.imshow('image',image)
-#cv2.waitKey(0)
-#cv2.destoryAllWindows()
+image=cv2.imread(s[0])
+res=cv2.resize(image,(500,500),interpolation=cv2.INTER_CUBIC)
+cv2.imshow('iker',res)
+cv2.waitKey(0)
+cv2.destoryAllWindows()
