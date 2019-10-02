3c5bd67fbe4bed3b3c890919a3f87ef383f5ed93
Willy Du <willy.du@ulictek.com>
2019-10-02 15:26:07 +0800

    Get filename extension.

--- process_image.py
+++ process_image.py
@@ -1,13 +1,15 @@
 import cv2
 import os
+import re
 path ="..\\waifu2x\\images" #Target of image file
 files = os.listdir(path) #Find all files in this file
 s=[]
 for file in files:
-    if os.path.exists(file):#Check wether this file is exist or not
-        file = open(file, 'r')#Read the file
-        print(file.readline())
-        file.close()
+    file_path = path+'\\'+file
+    if os.path.exists(file_path):#Check wether this file is exist or not
+        img_extension = os.path.splitext(file_path)[-1] #Get filename extension
+        if img_extension == '.png':
+            print(img_extension)
     else:
         print("Not exist")
 #image=cv2.imread('test.jpg')
