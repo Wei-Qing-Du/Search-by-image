69aa5ea11c62ff56220924592af104687a63c0e8
Do-Wie-Ching <qaz33338@gmail.com>
2019-10-27 22:25:16 +0800

    Update the English

--- CNN/PreWork.py
+++ CNN/PreWork.py
@@ -93,37 +93,31 @@ def get_files(file_dir,ratio):
     #print(val_labels)
 
     return tra_images,tra_labels,val_images,val_labels
-# To be continue
 #--------------------------生成batch------------------------
 
-#step1：将上面生成的list传入get_batch（），转换类型，产生一个输入队列queue，因为img和lab是分开的
-#所以使用tf.train.slice_input_producer(),然后用tf.read_file()从队列中读取图像
-#   image_w，image_H ：设置好固定的图像高度和宽度
-#   设置batch_size : 每个batch要放多少张图片
-#   capacity：一个队列最大多少
+#step1：Make the list introduce to the get_batch(), and change type，make a queue for img and lab.
+#So use tf.train.slice_input_producer(),and use tf.read_file() read images from queue.
+#   capacity：size of queue
 
 def get_batch(image,label,image_W,image_H,batch_size,capacity):
-    #用tf.cast()转换类型
-    image = tf.cast(image,tf.string)    #可变长度的字节数组，每一个张量元素都是一个字节数组
-    label = tf.cast(label,tf.int32)     #
+    #Use tf.cast() to change type
+    image = tf.cast(image,tf.string)    
+    label = tf.cast(label,tf.int32)    
 
 #    print(label)
-    # tf.train.slice_input_producer是一个tensor生成器
-    # 作用是按照设定，每次从一个tensor列表中按顺序或者随机抽取出一个tensor放入文件名队列。
+    # tf.train.slice_input_producer to make tensor
     #make an input queue
     input_queue = tf.train.slice_input_producer([image,label])
     label = input_queue[1]
-    image_contents = tf.read_file(input_queue[0])   # tf.read_file()从队列中读取图像
+    image_contents = tf.read_file(input_queue[0])   # tf.read_file()to read images
 
-    #step2:将图像解码，使用相同类型的图像。不同类型的图像不能混合在一起，要么只用JPEG，要么只用PNG等
+    #step2:Decode the images，that must use same type。
 #    image = tf.image.decode_image(image_contents,channels=3)
     image = tf.image.decode_jpeg(image_contents, channels=3)
-    # jpeg或者jpg格式都用decode_jpeg函数，其他格式可以去查看官方文档
 
-    #step3:数据预处理，对图像进行旋转、缩放、裁剪、归一化等操作，让计算出的图形更健壮
+    #step3:data preprocession
     image = tf.image.resize_image_with_crop_or_pad(image,image_W,image_H)
-    image = tf.image.per_image_standardization(image)   #对resize后的图片进行标准化处理
-
+    image = tf.image.per_image_standardization(image)   #Standard image after be resized
 #    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
     # if you want to test the generated batches of images, you might want to comment the following line.
     # 如果想看到正常的图片，请注释掉105行（标准化）和 121行（image_batch = tf.cast(image_batch, tf.float32)）
@@ -131,20 +125,20 @@ def get_batch(image,label,image_W,image_H,batch_size,capacity):
 #    image = tf.image.per_image_standardization(image)
 #具体解释地址看该链接代码：https://github.com/kevin28520/My-TensorFlow-tutorials/blob/master/01%20cats%20vs%20dogs/input_data.py
 
-    #step4:生成batch
+    #step4:Make the batch
     # image_batch: 4D tensor [batch_size, width, height, 3],dtype=tf.float32
     # label_batch: 1D tensor [batch_size], dtype=tf.int32
     image_batch ,label_batch = tf.train.batch([image,label],
                                               batch_size = batch_size,
                                               num_threads= 32,
                                               capacity =capacity)
-    #重新排列label，行数为[batch_size]
+    #Reshape label，row size is [batch_size]
     label_batch =tf.reshape(label_batch,[batch_size])
 #    image_batch = tf.cast(image_batch, tf.uint8)    # 显示彩色图像
     image_batch = tf.cast(image_batch,tf.float32)     #显示灰度图像
 
     return image_batch,label_batch
-    # 获取两个batch，两个batch即为传入神经网络的数据
+    # Get two batches that are introduced into the CNN.
 
 
 
