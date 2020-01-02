26933740ab2f53682fe9f86519b794b758df684d
Willy <willy.du@ulictek.com>
2020-01-02 18:33:50 +0800

    Add code

--- UI/SearchUI/SearchUI/Form1.cs
+++ UI/SearchUI/SearchUI/Form1.cs
@@ -12,6 +12,10 @@ namespace SearchUI
 {
     public partial class Form1 : Form
     {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
         public Form1()
         {
             InitializeComponent();
