47ff70c5eeab34870d69d72e498d9b5f25b769b8
Willy <willy.du@ulictek.com>
2020-01-02 18:22:40 +0800

    Use c# OpenCV package

--- UI/SearchUI/SearchUI/Program.cs
+++ UI/SearchUI/SearchUI/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+
 
 namespace SearchUI
 {
+   
     static class Program
     {
         /// <summary>
@@ -17,6 +20,7 @@ namespace SearchUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Mat aa;
         }
     }
 }
