4bdc3f5aaf2100c4b64f2f15393318f1a36d9c15
Do-Wie-Ching <qaz33338@gmail.com>
2020-02-20 00:48:59 +0800

    Modify code to be connected by C#.

--- predict_test.py
+++ predict_test.py
@@ -2,20 +2,41 @@
 import onnxruntime as rt
 import numpy as np
 from PIL import Image
+import sys
 
-# setup runtime - load the persisted ONNX model
-img = Image.open("C:\\Users\\Z97MX-GAMING\\Desktop\\train\\0_29.jpg")
-img.load()
-data = np.asarray( img, dtype="float32" )
-data = data.reshape(1, 3072)
 
-sess = rt.InferenceSession("model.onnx")
+def preprocess(path):
+    img = Image.open(path)
+    img = img.resize((32, 32))
 
-# get model metadata to enable mapping of new input to the runtime model.
-input_name = sess.get_inputs()[0].name
-label_name = sess.get_outputs()[0].name
+    return img
 
-# retrieve prediction - passing in the input list (you can also pass in multiple inputs as a list of lists)
-pred_onx = sess.run([label_name], {input_name: data})[0]
+def RunTestModel(img_path):
+    try:
+        # setup runtime - load the persisted ONNX model
+        img = preprocess(img_path)
+        img.load()
+    
+        data = np.asarray( img, dtype="float32" )
+        data = data.reshape(1, 3072)
+    
+        sess = rt.InferenceSession("..\\..\\..\\..\\..\\..\\model.onnx")
+    
 
-print(pred_onx)
+        # get model metadata to enable mapping of new input to the runtime model.
+        input_name = sess.get_inputs()[0].name
+        label_name = sess.get_outputs()[0].name
+
+        # retrieve prediction - passing in the input list (you can also pass in multiple inputs as a list of lists)
+        pred_onx = sess.run([label_name], {input_name: data})[0]
+
+    except Exception as e:
+        print(e)
+    return pred_onx
+
+
+
+
+
+
+print( RunTestModel(sys.argv[1]) )
